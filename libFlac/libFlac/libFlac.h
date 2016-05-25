cee9d8d669b1627dd5e7b3a0811ccf6cb926c358
Iftah Haimovitch <iftahh@gmail.com>
2016-05-25 14:25:52 +0300

    initial commit - libFLAC as iOS Framework

---
+++ libFlac/libFlac/libFlac.h
@@ -0,0 +1,20 @@
+//
+//  libFlac.h
+//  libFlac
+//
+//  Created by Iftah Haimovitch on 25/05/2016.
+//  Copyright © 2016 Evature. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libFlac.
+FOUNDATION_EXPORT double libFlacVersionNumber;
+
+//! Project version string for libFlac.
+FOUNDATION_EXPORT const unsigned char libFlacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libFlac/PublicHeader.h>
+
+
+#import "all.h"
